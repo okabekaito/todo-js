db61c13032024c37baba2cddda45be723468e31f
okabekaito <81666354+okabekaito@users.noreply.github.com>
2021-11-20 17:08:41 +0900

    inputAddDom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得して、空にする
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素の各要素を設定
+  div.appendChild(li);
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
